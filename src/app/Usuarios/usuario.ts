cf263ee66ebfe8d1d0aa23aabfc74c702c1e54cc
Maria Eduarda Ciriaco <45763711+duhmanenti@users.noreply.github.com>
2021-03-08 15:32:04 -0300

    Update usuario.ts
    mudança na classe usuário

--- src/app/Usuarios/usuario.ts
+++ src/app/Usuarios/usuario.ts
@@ -1,5 +1,4 @@
 export class Usuario {
-<<<<<<< HEAD
     nome? : string;
     sobrenome? : string;
     email? : string;
@@ -11,16 +10,3 @@ export class Usuario {
     cidade? : string;
     bairro? : string;
 } 
-=======
-        nome? : string;
-        sobrenome? : string;
-        email? : string;
-        senha? : string;
-        idade? : string;
-        telefone? : string;
-        foto? : string;
-        estado? : string;
-        cidade? : string;
-        endereco? : string;
-}
->>>>>>> 3351246ecc1aeb32415a39d723e6ce64d3dc2eea
