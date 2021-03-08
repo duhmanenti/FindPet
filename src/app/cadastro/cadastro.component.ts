7390b89df57a1d4f7a72b63a033f8a65ffa49e06
Maria Eduarda Ciriaco <45763711+duhmanenti@users.noreply.github.com>
2021-03-08 15:33:06 -0300

    Update cadastro.component.ts

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Usuario } from './../Usuarios/usuario';
-<<<<<<< HEAD
-=======
-
->>>>>>> 3351246ecc1aeb32415a39d723e6ce64d3dc2eea
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
+
 export class CadastroComponent implements OnInit {
   userCadastro : Usuario = {};
 
-  userCadastro : Usuario = {};
   constructor() { }
 
   ngOnInit(): void {
