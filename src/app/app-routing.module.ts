7e7176f6e61a6cd3d6f69af7a154ba11ac9ca1fd
Maria Eduarda <duhmanenti@gmail.com>
2021-03-09 14:45:45 -0300

    rotas da home p/ tela de login e cadastro; rota tela de login p/recuperar senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RecuperarsenhaComponent } from './login/recuperarsenha/recuperarsenha.component';
 
 const routes: Routes = [
-  {path: "login", component: LoginComponent},
-  {path: "cadastro", component: CadastroComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'recuperar-senha', component:RecuperarsenhaComponent},
+  {path:'', redirectTo: 'home', pathMatch:'full'}
 ];
 
 @NgModule({
